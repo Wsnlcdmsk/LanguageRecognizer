00d7d2d65f2562712afcbd878f93e809e82013ac
Wsnlcdmsk <134656590+Wsnlcdmsk@users.noreply.github.com>
2024-04-01 10:51:43 +0300

    code fixes

--- src/main/java/com/project/langrecognizer/service/ExternalApiService.java
+++ src/main/java/com/project/langrecognizer/service/ExternalApiService.java
@@ -16,8 +16,6 @@ import java.net.http.HttpResponse;
 public class ExternalApiService{
 
     private static final String API_URL = "https://api.apyhub.com/detect/language";
-    private static final String API_TOKEN = "APY0gCUDWWVG4oiF0p9D5Slbo4Ireht1hypbraaWggXoDyH7V5RjLlVUhdK0nDKH0A1nlI3";
-
     public Language detectLanguage(String text) {
         HttpClient client = HttpClient.newHttpClient();
 
@@ -25,7 +23,7 @@ public class ExternalApiService{
                 .uri(URI.create(API_URL))
                 .POST(HttpRequest.BodyPublishers.ofString("{\"text\": \"" + text + "\"}"))
                 .setHeader("Content-Type", "application/json")
-                .setHeader("apy-token", API_TOKEN)
+                .setHeader("apy-token", "APY0gCUDWWVG4oiF0p9D5Slbo4Ireht1hypbraaWggXoDyH7V5RjLlVUhdK0nDKH0A1nlI3")
                 .build();
 
         try {
@@ -42,7 +40,6 @@ public class ExternalApiService{
             language.setName("An error occurred");
             return language;
         } catch (Exception e) {
-            e.printStackTrace();
             Language language = new Language();
             language.setName("An error occurred");
             return language;
