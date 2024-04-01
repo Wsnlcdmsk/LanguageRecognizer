d61a2532900075f908edbe7ba5a9826627c21a38
Wsnlcdmsk <134656590+Wsnlcdmsk@users.noreply.github.com>
2024-04-01 10:48:39 +0300

     Please enter the commit message for your changes. Lines starting

--- src/main/java/com/project/langrecognizer/service/TextService.java
+++ src/main/java/com/project/langrecognizer/service/TextService.java
@@ -1,7 +1,5 @@
 package com.project.langrecognizer.service;
 
-import com.project.langrecognizer.model.Language;
-import com.project.langrecognizer.model.Tag;
 import com.project.langrecognizer.model.Text;
 
 import java.util.List;
